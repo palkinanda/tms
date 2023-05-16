da8a165e82a2fd259bc35ba2c5bdc1bfc8191534
Palki Nanda <you@example.com>
2023-05-17 03:18:36 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+<div class="copyrights">
+	<p>Made by students of Chandigarh University</p>
+	 <p>All Rights Reserved |  <a href="index.php">Intelligo</a> </p>
+</div>	
---
+++ indexs.php
@@ -0,0 +1,541 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intelligo|Chandigarh</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+    <!-- custom js file link  -->
+    <script src="js/script.js" defer></script>
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header" data-aos="fade-down">
+
+    <section class="flex">
+ 
+       <a href="index.php" class="logo">C chandigarh</a>
+ 
+       <nav class="navbar">
+        <a data-aos="zoom-in-left" data-aos-delay="300" href="indexs.php">home</a>
+        <a data-aos="zoom-in-left" data-aos-delay="450" href="about.html">about</a>
+        <a data-aos="zoom-in-left" data-aos-delay="600" href="tours.html">Tours</a>
+        <a data-aos="zoom-in-left" data-aos-delay="750" href="destinations.html">Destination</a>
+        <a data-aos="zoom-in-left" data-aos-delay="900" href="home.html">Login</a>
+        <a data-aos="zoom-in-left" data-aos-delay="1150" href="contact.html">Contact</a>
+    </nav>
+ 
+       <div id="menu-btn" class="fas fa-bars"></div>
+ 
+    </section>
+ 
+ </header>
+ 
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span data-aos="fade-up" data-aos-delay="150">Beautiful city</span>
+        <h3 data-aos="fade-up" data-aos-delay="300">chandigarh</h3>
+        <p data-aos="fade-up" data-aos-delay="450"></p>
+        <a data-aos="fade-up" data-aos-delay="600" href="#" class="btn">I am ready</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- booking form section starts  -->
+
+<section class="book-form" id="book-form">
+
+    <form action="">
+        <div data-aos="zoom-in" data-aos-delay="150" class="inputBox">
+            <span>from</span>
+            <input type="date" placeholder="place name" value="">
+        </div>
+        <div data-aos="zoom-in" data-aos-delay="300" class="inputBox">
+            <span>to</span>
+            <input type="date" value="">
+        </div>
+        <div data-aos="zoom-in" data-aos-delay="450" class="inputBox">
+            <span>number of guests</span>
+            <input type="text"   placeholder="number of travelers" value="">
+        </div>
+        <input data-aos="zoom-in" data-aos-delay="600" type="submit" value="Search" class="btn">
+    </form>
+
+</section>
+
+<!-- booking form section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <div class="video-container" data-aos="fade-right" data-aos-delay="300">
+        <video src="imagescu/chandigarh-4k-cinematic-drone-video-gopro.mp4" muted autoplay loop class="video"></video>
+        <div class="controls">
+            <span class="control-btn" data-src="images/about-vid-1.mp4"></span>
+            <span class="control-btn" data-src="images/about-vid-2.mp4"></span>
+            <span class="control-btn" data-src="images/about-vid-3.mp4"></span>
+        </div>
+    </div>
+
+    <div class="content" data-aos="fade-left" data-aos-delay="600">
+        <span>why Chandigarh??</span>
+        <h3> First planned city of india</h3>
+        <p>Perfect balance and combination of nature and technological advancements</p>
+        <a href="#" class="btn">read more</a>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- destination section starts  -->
+
+<section class="destination" id="destination">
+
+    <div class="heading">
+        <span>CHANDIGARH</span>
+        <h1>Explore the places in and near chandigarh</h1>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box" data-aos="fade-up" data-aos-delay="150">
+            <div class="image">
+                <img src="imagescu/pic1main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Morni hills</h3>
+                <p>Situated in the lower reaches of the Shivalik range, Morni is ideal for a holiday </p>
+                <a href="https://youtu.be/hOnDPdWMhBM"target="_blank">Get aerial/virtual view<i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="300">
+            <div class="image">
+                <img src="imagescu/pic2main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Rock garden</h3>
+                <p>Explore the creativity with rock designs </p>
+                <a href="https://youtu.be/CHqbDCcpL4g"target="_blank">Get aerial/virtual view<i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="450">
+            <div class="image">
+                <img src="imagescu/pic3main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Sukhna lake</h3>
+                <p>breathe the freshness of sukhna lake view with sunset</p>
+                <a href="https://youtu.be/kEg2OOPIuuU" target="_blank">Get aerial/virtual view <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="600">
+            <div class="image">
+                <img src="imagescu/pic4main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Zakir hussain rose garden</h3>
+                <p>Plantation of flowers of which you haven't even hear of</p>
+                <a href="https://youtu.be/FUZgn12R9xI"target="_blank">Get aerial/virtual view <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+        
+        <div class="box" data-aos="fade-up" data-aos-delay="750">
+            <div class="image">
+                <img src="imagescu/pic5main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>chandigarh university</h3>
+                <p>have glimpse of student college life in chandigarh</p>
+                <a href="https://youtu.be/j13Ms0Sb6TI"target="_blank">Get aerial/virtual view <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="900">
+            <div class="image">
+                <img src="imagescu/pic6 main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>shimla</h3>
+                <p>hill station? Obviously it's shimla</p>
+                <a href="https://youtu.be/JhtLehEjEeY"target="_blank">Get aerial/virtual view <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="1150">
+            <div class="image">
+                <img src="imagescu/pic7main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Amritsar</h3>
+                <p>wash of your sins at golden temple.</p>
+                <a href="https://youtu.be/IpZX5qkdxIM"target="_blank">Get aerial/virtual view<i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="1300">
+            <div class="image">
+                <img src="imagescu/pic8main.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>chandigarh mueseum</h3>
+                <p>Explore the heritage of chandigarh </p>
+                <a href="#">Get aerial/virtual view <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- destination section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <div class="heading">
+        <span>our services</span>
+        <h1>Always at your service</h1>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+            <i class="fas fa-globe"></i>
+            <h3>travel India</h3>
+            <p>travel and avail the services in any part of the country</p>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+            <i class="fas fa-hiking"></i>
+            <h3>ACtivities</h3>
+            <p>adventures and natural beauty of india </p>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+            <i class="fas fa-utensils"></i>
+            <h3>Cuisines</h3>
+            <p>Taste of india full of flavors </p>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="600">
+            <i class="fas fa-hotel"></i>
+            <h3>hotels</h3>
+            <p>comfortable and affordable stays for family and couples.</p>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="750">
+            <i class="fas fa-wallet"></i>
+            <h3>budget friendly</h3>
+            <p>We will take care of your wallet .</p>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="900">
+            <i class="fas fa-headset"></i>
+            <h3>Support</h3>
+            <p>If any query or complaints we will respond.</p>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- gallery section starts  -->
+
+<section class="gallery" id="gallery">
+
+    <div class="heading">
+        <span>Clicks by our </span>
+        <h1>Happy customer</h1>
+    </div>
+    
+    <div class="box-container">
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+            <img src="imagescu/img1gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+            <img src="imagescu/img2gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+            <img src="imagescu/img3gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+            <img src="imagescu/img4gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+            <img src="imagescu/img5gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+            <img src="imagescu/img6gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+            <img src="imagescu/img7gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+            <img src="imagescu/img8gallery .jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+            <img src="imagescu/img9gallery.jpg" alt="">
+            <span></span>
+            <h3></h3>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- gallery section ends -->
+
+<!-- review section starts  -->
+
+<section class="review">
+
+    <div class="content" data-aos="fade-right" data-aos-delay="300">
+        <span>Reviews</span>
+        <h3>from our customer</h3>
+        <p> pehle istemaal kre fir vishwas kare.</p>
+    </div>
+
+    <div class="box-container" data-aos="fade-left" data-aos-delay="600">
+
+        <div class="box">
+            <p>"I stumbled upon this travel guide website and it was a game-changer! The information was extensive, accurate, and made my bomb planning a breeze. Highly recommended!"</p>
+            <div class="user">
+                <img src="imagescu/osama.jpg" alt="">
+                <div class="info">
+                    <h3>osama bin laden </h3>
+                    <span>mastermind 9/11</span>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <p>"Amazing travel guide website! It helped me plan my 26/11 perfectly with great recommendations and tips. Highly recommend to anyone looking for an exceptional travel experience."</p>
+            <div class="user">
+                <img src="imagescu/daudabraham.jpg" alt="">
+                <div class="info">
+                    <h3>Daud ibraham</h3>
+                    <span>masters in bomb</span>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <p>"Amazing travel experience with this website! The bomb planting process was easy, and the trip was everything I hoped for and more. Highly recommend for anyone looking for a suicidal  planning."</p>
+            <div class="user">
+                <img src="imagescu/hafeezsaeed.jpg" alt="">
+                <div class="info">
+                    <h3>Hafeez saeed</h3>
+                    <span>Btech terrorism</span>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <p>website is a fantastic resource for any traveler. The website's user-friendly interface and insightful content make planning your next adventure a breeze. Highly recommend!"</p>
+            <div class="user">
+                <img src="imagescu/kanhiya lal.jpg" alt="">
+                <div class="info">
+                    <h3>kanhiya lal</h3>
+                    <span>anti nationalist</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <div class="heading">
+        <span>blogs</span>
+        <h1>experience by greats</h1>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box" data-aos="fade-up" data-aos-delay="150">
+            <div class="image">
+                <img src="imagescu/blogsimg1.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="link">Chandigarh city sunset view</a>
+                <p>perfect place when you want to kickback and relax with your loved ones. </p>
+                <div class="icon">
+                    <a href="#"><i class="fas fa-clock"></i> 20th feb 2023</a>
+                    <a href="#"><i class="fas fa-user"></i> by Narendra modi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="300">
+            <div class="image">
+                <img src="imagescu/blogsim2.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="link">Mohali </a>
+                <p>Greenerym stadium, infrastructure and activities it has all!!!</p>
+                <div class="icon">
+                    <a href="#"><i class="fas fa-clock"></i> 21st Feb 2023</a>
+                    <a href="#"><i class="fas fa-user"></i> Rahul gandhi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="450">
+            <div class="image">
+                <img src="imagescu/blogsimg3.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="link">Wagha border</a>
+                <p>Immense experienceto relive the border even after 70+ yrs of experience.</p>
+                <div class="icon">
+                    <a href="#"><i class="fas fa-clock"></i> 22nd feb 2023</a>
+                    <a href="#"><i class="fas fa-user"></i> Amitabh bacchan</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- banner section starts  -->
+
+<div class="banner">
+
+    <div class="content" data-aos="zoom-in-up" data-aos-delay="300">
+        <span>start your Journey now</span>
+        <h3>Let's Explore india like never before</h3>
+        <p>100+ cities are covered by us, join us and be like jeeyo toh har pal aise jeeyo jaise ki aakhri ho</p>
+        <a href="#book-form" class="btn">book now</a>
+    </div>
+
+</div>
+
+<!-- banner section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box" data-aos="fade-up" data-aos-delay="150">
+            
+            <p>follow us on our socials to know more about us</p>
+            <div class="share">
+                <a href="https://www.facebook.com/"target="_blank" class="fab fa-facebook-f"></a>
+                <a href="https://twitter.com/Chandigarh_uni"target="_blank" class="fab fa-twitter"></a>
+                <a href="https://www.instagram.com/chandigarhuniversity/?hl=en"target="_blank" class="fab fa-instagram"></a>
+                <a href="https://www.linkedin.com/school/chandigarh-university/"target="_blank" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="300">
+            <h3>follow up links</h3>
+            <a href="indexs.php" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="about.html" class="links"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="destinations.html" class="links"> <i class="fas fa-arrow-right"></i> destination </a>
+            <a href="tours.html" class="links"> <i class="fas fa-arrow-right"></i> tours </a>
+            <a href="home.html" class="links"> <i class="fas fa-arrow-right"></i> login</a>
+            <a href="contact.html" class="links"> <i class="fas fa-arrow-right"></i>contact us</a>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="450">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-map"></i> chandigarh,mumbai, india </p>
+            <p> <i class="fas fa-phone"></i> 8080867223</p>
+            <p> <i class="fas fa-envelope"></i>nikhilaryan2023@gmail.com</p>
+            <p> <i class="fas fa-clock"></i> 9:00am - 9:00pm </p>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="600">
+            <h3>Suggestions and complaints??</h3>
+            <p>please let us know</p>
+            <form action="">
+                <input type="text" name="" placeholder="complaints?we will help" class="email" id="">
+                <input type="submit" value="submit" class="btn">
+            </form>
+        </div>
+
+    </div>
+
+</section>
+
+<div class="credit">made by students of chandigarh university</div>
+<div class="credit">Frontend by nikhil jha</div>
+
+<!-- footer section ends -->
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<script>
+
+    AOS.init({
+        duration: 800,
+        offset:150,
+    });
+
+</script>
+
+</body>
+</html>
